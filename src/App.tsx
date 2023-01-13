286ce8fb5f39d293f01f81434e0b3683a664fe24
Marcin Sacha <marcindev03@gmail.com>
2023-01-13 22:08:50 +0100

    Setup portfolio section

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import type { Component } from "solid-js";
 
 import { Header, Technologies } from "./modules/homepage";
+import { PortfolioShowcase } from "./modules/portfolio";
 
 const App: Component = () => {
   return (
@@ -9,28 +10,7 @@ const App: Component = () => {
 
       <Technologies />
 
-      <section>
-        <div>
-          <h3 class="text-3xl py-1">Portfolio</h3>
-          <p class="text-md py-2 leading-8 text-gray-800">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Voluptatibus rem consectetur provident a repudiandae amet veniam ex
-            alias exercitationem, ratione dolorum voluptatum. Molestiae
-            consequatur perspiciatis labore illum nobis tenetur nostrum.
-          </p>
-        </div>
-        <div class="flex flex-col gap-10 py-10">
-          <div>
-            <img src="/web1.png" alt="" class="rounded-lg" />
-          </div>
-          <div>
-            <img src="/web2.png" alt="" class="rounded-lg" />
-          </div>
-          <div>
-            <img src="/web3.png" alt="" class="rounded-lg" />
-          </div>
-        </div>
-      </section>
+      <PortfolioShowcase />
 
       <section id="contact"></section>
     </main>
---
+++ src/modules/portfolio/PortfolioShowcase/PortfolioShowcase.tsx
@@ -0,0 +1,26 @@
+export const PortfolioShowcase = () => {
+  return (
+    <section>
+      <div>
+        <h3 class="text-3xl py-1">Portfolio</h3>
+        <p class="text-md py-2 leading-8 text-gray-800">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus
+          rem consectetur provident a repudiandae amet veniam ex alias
+          exercitationem, ratione dolorum voluptatum. Molestiae consequatur
+          perspiciatis labore illum nobis tenetur nostrum.
+        </p>
+      </div>
+      <div class="flex flex-col gap-10 py-10">
+        <div>
+          <img src="/web1.png" alt="" class="rounded-lg" />
+        </div>
+        <div>
+          <img src="/web2.png" alt="" class="rounded-lg" />
+        </div>
+        <div>
+          <img src="/web3.png" alt="" class="rounded-lg" />
+        </div>
+      </div>
+    </section>
+  );
+};
