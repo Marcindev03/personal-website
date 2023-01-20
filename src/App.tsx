324974c4371f2248aa1670c908651c22ddb0638e
Marcin Sacha <marcindev03@gmail.com>
2023-01-20 20:48:31 +0100

    Setup footer

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,23 @@ import type { Component } from "solid-js";
 import { ContactForm } from "./modules/contact";
 
 import { Header, Technologies } from "./modules/homepage";
+import { Footer } from "./modules/layout";
 import { PortfolioShowcase } from "./modules/portfolio";
 
 const App: Component = () => {
   return (
-    <main class={`bg-white px-10`}>
-      <Header />
+    <>
+      <main class={`bg-white px-10 pb-6`}>
+        <Header />
 
-      <Technologies />
+        <Technologies />
 
-      <PortfolioShowcase />
+        <PortfolioShowcase />
 
-      <ContactForm />
-    </main>
+        <ContactForm />
+      </main>
+      <Footer />
+    </>
   );
 };
 
